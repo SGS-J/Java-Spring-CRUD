0a519070f460db0d7544f8d8d107b8403b1ef36c
SGS-J <santigarsan07@gmail.com>
2022-09-03 16:00:38 -0500

    Change error message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const app = http.createServer((req, res) => {
   fs.readFile('./index.html', (err, data) => {
-    if (err) return res.end('Fatal error!');
+    if (err) return res.end('Super fatal error!');
     res.writeHead(200, { 'content-type': 'text/html' });
     return res.end(data);
   });
