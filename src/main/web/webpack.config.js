00ff8410f5155e4032019bcc8b9b3230eb9cf5d6
Santiago-GS <67868795+SGS-J@users.noreply.github.com>
2022-10-29 16:59:43 -0500

    Setup webpack dev mode

--- src/main/web/webpack.config.js
+++ src/main/web/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 
 module.exports = {
-    entry: './index.js',
+    entry: path.join(__dirname, 'index.js'),
     cache: true,
     mode: 'development',
     output: {
         path: path.join(__dirname, "../resources/static/built"),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        clean: true
     },
     module: {
         rules: [
